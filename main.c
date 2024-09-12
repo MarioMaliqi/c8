81e797e4b922f3a9a3a5f5d2340b0dec754361a7
Mario Maliqi <maliqimario@gmail.com>
2024-09-12 12:30:11 +0200

    changed single byte to short (2 bytes; 16 bit) for opcodes and converting the bytes from big endianess to little endianess. successfully printing opcodes to the terminal

--- main.c
+++ main.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <byteswap.h>
 
 char memory[4096];
 
 int main() {
   // intial stuff to do:
   // 1. load rom
+  FILE* rom;
+
+  rom = fopen("roms/Cave.ch8", "rb");
+
+  if (rom == NULL) {
+    fprintf(stderr, "ERROR: ROM could not be loaded");
+    return EXIT_FAILURE;
+  }
+
+  // allocate space for file
+  unsigned short *buffer; 
+  fseek(rom, 0, SEEK_END); 
+  int rom_len = ftell(rom);
+  fseek(rom, 0, SEEK_SET);
+  buffer = (short *)malloc(sizeof(short) * rom_len);
+
+  if (buffer == NULL) {
+    fprintf(stderr, "ERROR: Memory Error");
+    return EXIT_FAILURE;
+  }
+
+  // write to the buffer
+  fread(buffer, sizeof(short), rom_len, rom);
+  fclose(rom);
+
   // 2. loop through every 2 bytes
+  for (int opcode = 0; opcode < rom_len; opcode++) {
+    buffer[opcode] = bswap_16(buffer[opcode]);
+    printf("%02x\n", buffer[opcode]);
+  }
+
   // 3. print opcodes in right order
-  // get window open with raylib maybe
-  printf("hellow!\n");
-  return 0;
+  // and get window open with raylib maybe
+  
+  // 4. free stuff
+  free(buffer);
+
+  return EXIT_SUCCESS;
 }
